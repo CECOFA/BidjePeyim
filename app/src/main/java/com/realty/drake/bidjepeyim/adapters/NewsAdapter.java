c180d75b33e47e644b98b844411be8fa7b3f787d
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-05 22:32:18 -0400

    recyclerView library added, News Adapter created
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/adapters/NewsAdapter.java
@@ -0,0 +1,9 @@
+package com.realty.drake.bidjepeyim.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by drake on 8/5/18
+ */
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+}
