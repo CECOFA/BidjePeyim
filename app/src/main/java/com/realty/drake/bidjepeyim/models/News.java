94a11d6052a52c452770306b6fb92d9e87546419
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-09 15:54:40 -0400

    BidjePeyim-19 Create News model class News model class created, variables added,Constructor added
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/models/News.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/models/News.java
@@ -1,8 +1,33 @@
 package com.realty.drake.bidjepeyim.models;
 
+import org.parceler.Parcel;
+
 /**
  * Created by drake on 8/8/18
  */
+@Parcel
 public class News {
     //todo Implement News model, using parceler library
+    private double idActualite;
+    private String titreActualite;
+    private String contenuActualite;
+    private String motsCles;
+    private String datePublication;
+    private String auteur;
+
+    public News() { } //Needed for Firebase's auto data mapping
+
+    public News(double idActualite,
+                String titreActualite,
+                String contenuActualite,
+                String motsCles,
+                String datePublication,
+                String auteur) {
+        this.idActualite = idActualite;
+        this.titreActualite = titreActualite;
+        this.contenuActualite = contenuActualite;
+        this.motsCles = motsCles;
+        this.datePublication = datePublication;
+        this.auteur = auteur;
+    }
 }
