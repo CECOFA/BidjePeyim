0308956c5fca8d71a8b5ba2373072c98755918ef
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-08 22:06:26 -0400

    RegulatoryFrameworkFragment created

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/RegulatoryFrameworkFragment.java
@@ -0,0 +1,10 @@
+package com.realty.drake.bidjepeyim.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by drake on 8/8/18
+ */
+public class RegulatoryFrameworkFragment extends Fragment {
+    //todo Implement RegulatoryFrameworkFragment here
+}
