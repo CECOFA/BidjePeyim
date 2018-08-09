0148f3407dadf2b04c46e13b7d3eee11887b87c6
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-08 23:14:24 -0400

    Search Activity created

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.realty.drake.bidjepeyim.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.realty.drake.bidjepeyim.R;
+
+public class SearchActivity extends AppCompatActivity {
+    //todo Implement the Search Activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
