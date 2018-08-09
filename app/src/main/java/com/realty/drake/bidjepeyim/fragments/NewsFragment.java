72d27d64ef8c3f658c5732065fa5f57ca78b5918
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-08 21:55:07 -0400

    NewsFragemnt created

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
@@ -0,0 +1,10 @@
+package com.realty.drake.bidjepeyim.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by drake on 8/8/18
+ */
+public class NewsFragment extends Fragment{
+    //Implement the fragment related to the News
+}
