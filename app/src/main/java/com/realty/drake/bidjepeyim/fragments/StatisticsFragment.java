99a1e7c4552e6a0221d16a5fee57c24b56e683cd
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-10 10:58:32 -0400

    BidjePeyim-29 Implement Pager Adapter in Main activity tab title scroll and title fully displayed
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/fragments/StatisticsFragment.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/StatisticsFragment.java
@@ -1,10 +1,24 @@
 package com.realty.drake.bidjepeyim.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.realty.drake.bidjepeyim.R;
 
 /**
  * Created by drake on 8/8/18
  */
 public class StatisticsFragment extends Fragment {
     //todo Implement StatisticsFragment here
+    //todo replace layout
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater
+                .inflate(R.layout.activity_about, container, false);
+    }
+
 }
