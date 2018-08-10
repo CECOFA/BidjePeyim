afcea2f8985c8022d6fd6b610759e1f28be98808
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-10 12:22:02 -0400

    BidjePeyim-27 Implement News Fragment, talk to the backend Read value LALALA from firebase, and display it in Toast and textView
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
@@ -1,14 +1,23 @@
 package com.realty.drake.bidjepeyim.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.realty.drake.bidjepeyim.R;
+import com.realty.drake.bidjepeyim.models.News;
 
 /**
  * Created by drake on 8/8/18
@@ -28,12 +37,29 @@ public class NewsFragment extends Fragment{
      * @param savedInstanceState If non-null, this fragment is being re-constructed
      */
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         NewsRef = FirebaseDatabase.getInstance()
                 .getReference()
-                .child("Buy");
+                .child("News");
         NewsRef.keepSynced(true);
+
+        // Read from the database
+        NewsRef.child("1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                News news = dataSnapshot.getValue(News.class);
+                assert news != null;
+                Toast.makeText(getContext(), news.getAuteur(), Toast.LENGTH_SHORT).show();
+                TextView textView = view.findViewById(R.id.tvAbout);
+                textView.setText(news.getAuteur());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     /**
@@ -57,6 +83,7 @@ public class NewsFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater
+                .inflate(R.layout.activity_about, container, false);
     }
 }
