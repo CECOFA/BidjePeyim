8dd2e9592ec3e7e17eb634cbc3bfb4e231c4bc1f
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-09 22:06:29 -0400

    BidjePeyim-28 Implement FragmentPagerAdapter
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/adapters/BidjePagerAdapter.java
@@ -0,0 +1,67 @@
+package com.realty.drake.bidjepeyim.adapters;
+
+/*
+ * Created by drake on 8/9/18
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+import com.realty.drake.bidjepeyim.fragments.BudgetFragment;
+import com.realty.drake.bidjepeyim.fragments.NewsFragment;
+import com.realty.drake.bidjepeyim.fragments.RegulatoryFrameworkFragment;
+import com.realty.drake.bidjepeyim.fragments.StatisticsFragment;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class BidjePagerAdapter extends FragmentPagerAdapter {
+
+
+    public BidjePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Log.d("DEBUGG","getItem called in FragmentPagerAdapter" );
+        //Returning the current tab
+        switch (position) {
+            case 0:
+                return new NewsFragment();
+            case 1:
+                return new BudgetFragment();
+            case 2:
+                return new StatisticsFragment();
+            case 3:
+                return new RegulatoryFrameworkFragment();
+            default:
+                return  null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        // Show 4 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        Log.d("Debugg", "getPageTitle: Executed");
+        switch (position) {
+            case 0:
+                return "Actualites";
+            case 1:
+                return "Budget";
+            case 2:
+                return "Statistiques";
+            case 3:
+                return "Cadre Legale";
+        }
+        return null;
+    }
+}
