28217920611fbde92faef6a2da531bd9f87c1444
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-10 12:34:50 -0400

    BidjePeyim-27 Implement News Fragment, talk to the backend onCreateView modified
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +26,16 @@ import com.realty.drake.bidjepeyim.models.News;
 public class NewsFragment extends Fragment{
     //todo Implement the fragment related to the News
     private DatabaseReference NewsRef;
+    private RecyclerView rvNews;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater
+                .inflate(R.layout.fragment_news, container, false);
+        rvNews = rootView.findViewById(R.id.rv_News);
+        return rootView;
+    }
 
     /**
      * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
@@ -62,28 +73,4 @@ public class NewsFragment extends Fragment{
         });
     }
 
-    /**
-     * Called to have the fragment instantiate its user interface view.
-     * This is optional, and non-graphical fragments can return null (which
-     * is the default implementation).  This will be called between
-     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
-     * <p>
-     * <p>If you return a View from here, you will later be called in
-     * {@link #onDestroyView} when the view is being released.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate
-     *                           any views in the fragment,
-     * @param container          If non-null, this is the parent view that the fragment's
-     *                           UI should be attached to.  The fragment should not add the view itself,
-     *                           but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     *                           from a previous saved state as given here.
-     * @return Return the View for the fragment's UI, or null.
-     */
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater
-                .inflate(R.layout.activity_about, container, false);
-    }
 }
