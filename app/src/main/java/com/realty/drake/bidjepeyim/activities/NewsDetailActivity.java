fc0e3ffb6fab3e291cc2c42190adf96c52acdf7a
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-11 14:17:25 -0400

    BidjePeyim-32 Implement News Details view NewsDetailActivity created
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/NewsDetailActivity.java
@@ -0,0 +1,15 @@
+package com.realty.drake.bidjepeyim.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.realty.drake.bidjepeyim.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
