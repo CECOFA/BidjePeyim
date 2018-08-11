c0de4a09b9af0c9bf12edea6578a6a0d4114d85d
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-11 14:26:24 -0400

    BidjePeyim-32 Implement News Details view Get parcel
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/activities/NewsDetailActivity.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/NewsDetailActivity.java
@@ -4,12 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.realty.drake.bidjepeyim.R;
+import com.realty.drake.bidjepeyim.models.News;
+
+import org.parceler.Parcels;
 
 public class NewsDetailActivity extends AppCompatActivity {
+    News news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        //Get Parcel
+        news = Parcels
+                .unwrap(getIntent()
+                        .getParcelableExtra("news"));
     }
 }
