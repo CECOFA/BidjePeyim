35609ec3004c4a1c660d1bc08f2fde2de205b8b6
ledanfab.93@gmail.com <ledanfab.93@gmail.com>
2018-08-11 14:22:52 -0800

    Adding lottie files for animation
    Signed-off-by: ledanfab.93@gmail.com <ledanfab.93@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/NewsFragment.java
@@ -1,24 +1,39 @@
 package com.realty.drake.bidjepeyim.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.realty.drake.bidjepeyim.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by drake on 8/8/18
  */
 public class NewsFragment extends Fragment{
     //todo Implement the fragment related to the News
-    //todo replace layout
+    private DatabaseReference NewsRef;
+
+    /**
+     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     *
+     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater
-                .inflate(R.layout.activity_about, container, false);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NewsRef = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("Buy");
+        NewsRef.keepSynced(true);
     }
 
 }
