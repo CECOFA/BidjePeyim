ebebee3696cfdaa744efec02f8e67623d293f6b8
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-15 21:34:57 -0400

    BidjePeyim-47 Budget Fragment - Add ChildList and ParentList class MyParentViewHolder created
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/bidjepeyim/fragments/MyParentChildHolder.java
@@ -0,0 +1,25 @@
+package com.realty.drake.bidjepeyim.fragments;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
+import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder;
+
+/**
+ * Created by drake on 8/15/18
+ */
+public class MyParentViewHolder extends GroupViewHolder {
+
+    public TextView listGroup;
+
+    public MyParentViewHolder(View itemView) {
+        super(itemView);
+        listGroup = itemView.findViewById(R.id.listParent);
+    }
+
+    public void setParentTitle(ExpandableGroup group) {
+        listGroup.setText(group.getTitle());
+    }
+}
+
