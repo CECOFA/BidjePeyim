28b96ac4cff99ba5ad2af3356691602de85902c5
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-18 18:43:33 -0400

    BidjePeyim-55 Navigation Drawer - Create Nav Drawer UI nav drawer text highlight, java code added
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/activities/MainActivity.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/MainActivity.java
@@ -1,18 +1,36 @@
 package com.realty.drake.bidjepeyim.activities;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.realty.drake.bidjepeyim.R;
 import com.realty.drake.bidjepeyim.adapters.BidjePagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    return true;
+                });
+
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
