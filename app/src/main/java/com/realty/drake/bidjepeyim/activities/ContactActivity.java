68a64517d40b0d33c7a19902be47d92cffb08e70
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-20 18:54:03 -0400

    Activity parent set for About us
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/activities/ContactActivity.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/ContactActivity.java
@@ -1,7 +1,14 @@
 package com.realty.drake.bidjepeyim.activities;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.realty.drake.bidjepeyim.R;
@@ -9,13 +16,47 @@ import com.realty.drake.bidjepeyim.adapters.ContactsAdapter;
 import com.realty.drake.bidjepeyim.models.Contacts;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ContactActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Toolbar toolbar = findViewById(R.id.bidje_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        Objects.requireNonNull(actionbar).setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_about: {
+                            startActivity(new Intent
+                                    (this, AboutActivity.class));
+                            break;
+                        }
+                        case R.id.nav_contacts: {
+                            startActivity(new Intent
+                                    (this, ContactActivity.class));
+                            break;
+                        }
+
+                    }
+                    return true;
+                });
 
         //Construct the data source
         ArrayList<Contacts> arrayOfUsers = new ArrayList<Contacts>();
@@ -43,5 +84,14 @@ public class ContactActivity extends AppCompatActivity {
         //Attach the adapter to the ListView
         ListView listView = (ListView) findViewById(R.id.lvItems);
         listView.setAdapter(adapter);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
