76631a91065022fcb96c720516c97f7dfa39b19c
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-23 18:06:53 -0400

    App bar added in News Detail
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/bidjepeyim/activities/NewsDetailActivity.java
+++ app/src/main/java/com/realty/drake/bidjepeyim/activities/NewsDetailActivity.java
@@ -1,7 +1,13 @@
 package com.realty.drake.bidjepeyim.activities;
 
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +17,8 @@ import com.realty.drake.bidjepeyim.R;
 import com.realty.drake.bidjepeyim.models.News;
 
 import org.parceler.Parcels;
+
+import java.util.Objects;
 
 public class NewsDetailActivity extends AppCompatActivity {
     News news;
@@ -25,6 +33,11 @@ public class NewsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        Toolbar toolbar = findViewById(R.id.bidje_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        Objects.requireNonNull(actionbar).setDisplayHomeAsUpEnabled(true);
 
         //Get Parcel
         news = Parcels
@@ -47,5 +60,24 @@ public class NewsDetailActivity extends AppCompatActivity {
                     .load(news.getImageActualite())
                     .into(imageActualite);
         }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK)
+        {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+        return true;
     }
 }
